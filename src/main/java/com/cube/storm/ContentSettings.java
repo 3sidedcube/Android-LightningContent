6d74ba835aa10638ee602b92055f9c3d1776a0e9
Callum Taylor <callum@3sidedcube.com>
2014-10-06 17:14:51 +0100

    Initial project setup

---
+++ src/main/java/com/cube/storm/ContentSettings.java
@@ -0,0 +1,77 @@
+package com.cube.storm;
+
+import android.content.Context;
+
+/**
+ * This is the entry point class of the library. To enable the use of the library, you must instantiate
+ * a new {@link com.cube.storm.ContentSettings.Builder} object in your {@link android.app.Application} singleton class.
+ *
+ * This class should not be directly instantiated.
+ *
+ * @author Callum Taylor
+ * @project StormContent
+ */
+public class ContentSettings
+{
+	/**
+	 * The singleton instance of the settings
+	 */
+	private static ContentSettings instance;
+
+	/**
+	 * Gets the instance of the {@link com.cube.storm.ContentSettings} class
+	 * Throws a {@link IllegalAccessError} if the singleton has not been instantiated properly
+	 *
+	 * @return The instance
+	 */
+	public static ContentSettings getInstance()
+	{
+		if (instance == null)
+		{
+			throw new IllegalAccessError("You must build the Ui settings object first using UiSettings$Builder");
+		}
+
+		return instance;
+	}
+
+	/**
+	 * Default private constructor
+	 */
+	private ContentSettings(){}
+
+	/**
+	 * The builder class for {@link com.cube.storm.ContentSettings}. Use this to create a new {@link com.cube.storm.ContentSettings} instance
+	 * with the customised properties specific for your project.
+	 *
+	 * Call {@link #build()} to build the settings object.
+	 */
+	public static class Builder
+	{
+		/**
+		 * The temporary instance of the {@link com.cube.storm.ContentSettings} object.
+		 */
+		private ContentSettings construct;
+
+		private Context context;
+
+		/**
+		 * Default constructor
+		 */
+		public Builder(Context context)
+		{
+			this.construct = new ContentSettings();
+			this.context = context.getApplicationContext();
+		}
+
+		/**
+		 * Builds the final settings object and sets its instance. Use {@link #getInstance()} to retrieve the settings
+		 * instance.
+		 *
+		 * @return The newly set {@link com.cube.storm.ContentSettings} instance
+		 */
+		public ContentSettings build()
+		{
+			return (ContentSettings.instance = construct);
+		}
+	}
+}
