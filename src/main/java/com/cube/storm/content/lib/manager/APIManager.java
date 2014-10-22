eba26488d06be52f0a6f2a0238d1c7a151547ea5
Callum Taylor <callum@3sidedcube.com>
2014-10-22 09:46:13 +0100

    Tidied up APIManager and updated dependancies

--- src/main/java/com/cube/storm/content/lib/manager/APIManager.java
+++ src/main/java/com/cube/storm/content/lib/manager/APIManager.java
@@ -1,25 +1,28 @@
 package com.cube.storm.content.lib.manager;
 
-import android.text.TextUtils;
-
-import com.cube.storm.content.ModuleSettings;
-import com.cube.storm.content.ModuleSettings.Environment;
+import com.cube.storm.ContentSettings;
 import com.cube.storm.content.lib.Constants;
-import com.cube.storm.content.lib.D;
 
 import net.callumtaylor.asynchttp.AsyncHttpClient;
-import net.callumtaylor.asynchttp.response.AsyncHttpResponseHandler;
 import net.callumtaylor.asynchttp.response.JsonResponseHandler;
 
-import org.apache.http.Header;
-import org.apache.http.message.BasicHeader;
-
-import java.util.ArrayList;
-
+/**
+ * This is the manager class responsible for checking for and downloading updates from the server
+ *
+ * This class should not be directly instantiated.
+ *
+ * @author Callum Taylor
+ * @project StormContent
+ */
 public class APIManager
 {
 	private static APIManager instance;
 
+	/**
+	 * Gets the api manager singleton or creates one if its null
+	 *
+	 * @return The api manager singleton
+	 */
 	public static APIManager getInstance()
 	{
 		if (instance == null)
@@ -36,53 +39,83 @@ public class APIManager
 		return instance;
 	}
 
-	public void checkForUpdate(int appId, long lastUpdate, String density, JsonResponseHandler response)
-	{
-		checkForUpdate(appId, lastUpdate, false, density, response);
-	}
+	private APIManager(){}
 
-	public void checkForUpdate(int appId, long lastUpdate, boolean inTest, String density, JsonResponseHandler response)
+	/**
+	 * Checks the API for any delta updates.
+	 *
+	 * Uses the URLs defined in {@link com.cube.storm.ContentSettings#contentBaseUrl} and {@link com.cube.storm.ContentSettings#contentVersion} to check for
+	 * updates since {@param lastUpdate}
+	 *
+	 * @param lastUpdate The time of the last update. Usually found in the {@code manifest.json} file
+	 * @param response The response to use for downloading the delta
+	 */
+	public void checkForDelta(long lastUpdate, JsonResponseHandler response)
 	{
-		Environment environment = inTest ? Environment.TEST : ModuleSettings.CONTENT_ENVIRONMENT;
-		String urlPart = String.format(Constants.API_CONTENT_UPDATE, appId, lastUpdate, density, environment.getEnvironmentLabel());
-		D.out("Checking updates %s%s", Constants.API_CONTENT_URL, urlPart);
+		String appId = "";
 
-		ArrayList<Header> headers = new ArrayList<Header>();
-		if (!TextUtils.isEmpty(ModuleSettings.CONTENT_AUTH_TOKEN))
+		try
 		{
-			headers.add(new BasicHeader("Authorization", ModuleSettings.CONTENT_AUTH_TOKEN));
+			appId = ContentSettings.getInstance().getAppId().split("_")[2];
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+			throw new Error("App ID set in ContentSettings$appId is an incorrect format");
 		}
 
-		AsyncHttpClient client = new AsyncHttpClient(Constants.API_CONTENT_URL);
+		String urlPart = String.format(Constants.API_CONTENT_UPDATE, appId, lastUpdate, ContentSettings.getInstance().getContentEnvironment().getEnvironmentLabel());
+
+		AsyncHttpClient client = new AsyncHttpClient(ContentSettings.getInstance().getContentBaseUrl());
 		client.setAllowRedirect(false);
-		client.get(urlPart, null, headers, response);
+		client.get(ContentSettings.getInstance().getContentVersion() + "/" + urlPart, response);
 	}
 
-	public void checkForBundle(int appId, long lastUpdate, boolean inTest, String density, JsonResponseHandler response)
+	/**
+	 * Downloads a full bundle from the server
+	 *
+	 * Uses the URLs defined in {@link com.cube.storm.ContentSettings#contentBaseUrl} and {@link com.cube.storm.ContentSettings#contentVersion} to download a full
+	 * bundle
+	 *
+	 * @param response The response to use for downloading the bundle
+	 */
+	public void checkForBundle(JsonResponseHandler response)
 	{
-		Environment environment = inTest ? Environment.TEST : ModuleSettings.CONTENT_ENVIRONMENT;
-		String urlPart = String.format(Constants.API_BUNDLE, appId, density, environment.getEnvironmentLabel());
-		D.out("Checking updates %s%s", Constants.API_CONTENT_URL, urlPart);
+		checkForBundle(-1, response);
+	}
 
-		ArrayList<Header> headers = new ArrayList<Header>();
-		if (!TextUtils.isEmpty(ModuleSettings.CONTENT_AUTH_TOKEN))
+	/**
+	 * Downloads a full bundle from the server
+	 *
+	 * Uses the URLs defined in {@link com.cube.storm.ContentSettings#contentBaseUrl} and {@link com.cube.storm.ContentSettings#contentVersion} to download a full
+	 * bundle since {@param lastUpdate}
+	 *
+	 * @param lastUpdate The time of the last update. Usually found in the {@code manifest.json} file
+	 * @param response The response to use for downloading the bundle
+	 */
+	public void checkForBundle(long lastUpdate, JsonResponseHandler response)
+	{
+		String appId = "";
+
+		try
 		{
-			headers.add(new BasicHeader("Authorization", ModuleSettings.CONTENT_AUTH_TOKEN));
+			appId = ContentSettings.getInstance().getAppId().split("_")[2];
 		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+			throw new Error("App ID set in ContentSettings$appId is an incorrect format");
+		}
+
+		String urlPart = String.format(Constants.API_BUNDLE, appId, ContentSettings.getInstance().getContentEnvironment().getEnvironmentLabel());
 
 		if (lastUpdate > -1)
 		{
 			urlPart += "&timestamp=" + lastUpdate;
 		}
 
-		AsyncHttpClient client = new AsyncHttpClient(Constants.API_CONTENT_URL);
+		AsyncHttpClient client = new AsyncHttpClient(ContentSettings.getInstance().getContentBaseUrl());
 		client.setAllowRedirect(false);
-		client.get(urlPart, null, headers, response);
-	}
-
-	public void downloadUpdate(String endpoint, AsyncHttpResponseHandler response)
-	{
-		AsyncHttpClient client = new AsyncHttpClient(endpoint);
-		client.get(response);
+		client.get(ContentSettings.getInstance().getContentVersion() + "/" + urlPart, response);
 	}
 }
