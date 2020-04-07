6e94978b8f3bff75e7407f04a7829c911379584b
Tim Matthews <tim@3sidedcube.com>
2020-04-07 09:18:40 +0100

    Moved content migration logic over from develop library, as it is not strictly-development related and is a general concern

--- library/src/main/java/com/cube/storm/content/lib/helper/BundleHelper.java
+++ library/src/main/java/com/cube/storm/content/lib/helper/BundleHelper.java
@@ -6,9 +6,36 @@ import com.cube.storm.content.model.Manifest;
 
 public class BundleHelper
 {
+	/**
+	 * Deletes all files in {@link ContentSettings#getStoragePath()}
+	 */
+	public static void clearCache()
+	{
+		String path = ContentSettings.getInstance().getStoragePath();
+		FileHelper.deleteRecursive(new File(path, "pages/"));
+		FileHelper.deleteRecursive(new File(path, "data/"));
+		FileHelper.deleteRecursive(new File(path, "content/"));
+		FileHelper.deleteRecursive(new File(path, "languages/"));
+		FileHelper.deleteRecursive(new File(path, "app.json"));
+		FileHelper.deleteRecursive(new File(path, "manifest.json"));
+	}
+
 	public static Long readContentTimestamp()
 	{
 		Uri manifestUri = Uri.parse("cache://manifest.json");
+		Manifest manifest = ContentSettings.getInstance().getBundleBuilder().buildManifest(manifestUri);
+
+		if (manifest == null)
+		{
+			return null;
+		}
+
+		return manifest.getTimestamp();
+	}
+
+	public static Long readInitialTimestamp()
+	{
+		Uri manifestUri = Uri.parse("assets://manifest.json");
 		Manifest manifest = ContentSettings.getInstance().getBundleBuilder().buildManifest(manifestUri);
 
 		if (manifest == null)
